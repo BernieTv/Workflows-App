1e284580f2eb36dd69596b1aca16739479f4554d
Bekzod Tukhtasinov <86732898+BernieTv@users.noreply.github.com>
2024-12-23 15:04:08 +0500

    Update handleCheckoutSessionCompleted.ts

--- lib/stripe/handleCheckoutSessionCompleted.ts
+++ lib/stripe/handleCheckoutSessionCompleted.ts
@@ -6,6 +6,8 @@ import Stripe from "stripe";
 export async function HandleCheckoutSessionCompleted(
   event: Stripe.Checkout.Session
 ) {
+  console.log('SUCCESS WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW')
+  
   if (!event.metadata) {
     throw new Error("missing metadata");
   }
